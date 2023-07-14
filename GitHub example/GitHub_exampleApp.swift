a334dff3b84d4e6314c5a19dbd80e86876997085
Gina Lobaito <lobaitogina@gmail.com>
2023-07-14 10:16:30 -0400

    Initial Commit

---
+++ GitHub example/GitHub_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_exampleApp.swift
+//  GitHub example
+//
+//  Created by Gina Lobaito on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
